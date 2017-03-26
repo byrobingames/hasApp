4a9631cbc307ecc2f8e6944ccdc1bf31391cea8f
Robin Schaafma <info@byrobingames.com>
2017-03-26 10:38:25 +0200

    Updated to gitlab

---
+++ HasApp.hx
@@ -0,0 +1,92 @@
+package;
+
+#if cpp
+import cpp.Lib;
+#elseif neko
+import neko.Lib;
+#else
+import openfl.Lib;
+#end
+
+#if android
+import openfl.utils.JNI;
+#end
+
+class HasApp
+{
+    #if android
+    private static var ANDROID_CLASS:String = "com/byrobin/hasApp/HasApp";
+
+	private static var __checkpackage_android:Dynamic;
+	private static var __checkappname_android:Dynamic;		
+	public static var _package:Array<Dynamic>;
+	public static var _appnames:Array<Dynamic>;
+	
+	private function new() {
+	
+		_package =[];
+		_appnames =[];
+	
+	}
+	
+	public static function checkPackage() {	
+        	try {		
+				trace("CheckPackage");			
+       	     	__checkpackage_android = JNI.createStaticMethod(ANDROID_CLASS, "checkPackage", "(Lorg/haxe/lime/HaxeObject;)V", true);
+
+       	    	var args = new Array<Dynamic>();
+				args.push(new HasApp());
+            	__checkpackage_android(args);
+			} catch (e:Dynamic) {
+       			trace("Could detect if an package is installed");
+        	}
+		
+    }
+	
+	public static function getInstalledPackage():Array<Dynamic> {
+        
+        return _package;
+    }
+	
+	public static function checkAppName() {	
+        	try {		
+				trace("CheckAppname");			
+       	     	__checkappname_android = JNI.createStaticMethod(ANDROID_CLASS, "checkAppName", "(Lorg/haxe/lime/HaxeObject;)V", true);
+
+       	    	var args = new Array<Dynamic>();
+				args.push(new HasApp());
+            	__checkappname_android(args);
+			} catch (e:Dynamic) {
+       			trace("Could detect if an package is installed");
+        	}
+		
+    }
+	
+	public static function getInstalledAppname():Array<Dynamic> {
+        
+        return _appnames;
+    }
+	
+	
+	/////////////Android Event	
+	
+	public function isPackageInstalled(packages:String = "") {
+		
+		var p = packages.split(",");
+		_package = p;
+		
+		//trace("Package " + _package);
+    }
+	
+	public function isAppInstalled(appnames:String) {
+	
+		var a = appnames.split(",");
+		_appnames = a;
+		
+		//trace("AppName: " + _appnames);
+    }
+		
+#end
+
+
+}
---
+++ dependencies/hasApp/src/com/byrobin/hasApp/HasApp.java
@@ -0,0 +1,94 @@
+package com.byrobin.hasApp;
+
+import org.haxe.extension.Extension;
+import org.haxe.lime.HaxeObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.util.Log;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import android.widget.ArrayAdapter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import java.lang.Thread;
+
+
+public class HasApp extends Extension
+{
+    private static HaxeObject callback;
+    private static PackageManager pm;
+
+    public static void checkPackage(final HaxeObject cb){
+        
+        callback = cb;
+    
+        mainActivity.runOnUiThread(new Runnable() {
+            public void run() {
+                
+                pm = mainContext.getPackageManager();
+                List<ApplicationInfo> packageList = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+                ArrayList<String> installedapplist = new ArrayList<String>();
+                
+                Iterator<ApplicationInfo> it = packageList.iterator();
+                while(it.hasNext()){
+                    ApplicationInfo pk=(ApplicationInfo)it.next();
+                    
+                    String packageName=pk.packageName.toString();
+                    
+                    Log.d("HasApp","PackageName:" + packageName);
+                    
+                    installedapplist.add(packageName);
+                    
+                }
+                
+                String finalPackages= installedapplist.toString();
+                
+                callback.call("isPackageInstalled", new Object[] {finalPackages.replace(", ", ",").replace("]", "").replace("[", "")});
+            }
+        });
+    }
+    
+    public static void checkAppName(final HaxeObject cb){
+        
+        callback = cb;
+        
+        mainActivity.runOnUiThread(new Runnable() {
+            public void run() {
+                
+                pm = mainContext.getPackageManager();
+                List<ApplicationInfo> appList = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+                ArrayList<String> installedapplist = new ArrayList<String>();
+                
+                Iterator<ApplicationInfo> it = appList.iterator();
+                while(it.hasNext()){
+                    ApplicationInfo pk=(ApplicationInfo)it.next();
+                    
+                    String appName = pm.getApplicationLabel(pk).toString();
+                    
+                    //Log.d("HasApp","AppName:" + appName);
+                    
+                    installedapplist.add(appName);
+                }
+                
+                String finalAppNames= installedapplist.toString();
+                
+                callback.call("isAppInstalled", new Object[] {finalAppNames.replace(", ", ",").replace("]", "").replace("[", "")});
+                
+            }
+        });
+    }
+    
+    
+}
